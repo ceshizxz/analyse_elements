a181c724a9758855ab4878f29e5958d95bf152e0
ceshizxz <ceshizxzzz@gmail.com>
2025-06-02 21:11:33 +0800

    Modified to keep black shapes instead of removing the white background.

--- other/remove_background.py
+++ other/remove_background.py
@@ -1,23 +1,27 @@
 import os
 from PIL import Image
 
-def remove_white_background(image_path, output_path):
+def keep_dark_lines_only(image_path, output_path, threshold=160):
     img = Image.open(image_path).convert("RGBA")
     datas = img.getdata()
 
     newData = []
     for item in datas:
-        if item[0] > 240 and item[1] > 240 and item[2] > 240:
-            newData.append((255, 255, 255, 0))
+        r, g, b, a = item
+        gray = (r + g + b) / 3
+        if gray < threshold:
+            newData.append((0, 0, 0, 255))
         else:
-            newData.append(item)
-
+            newData.append((255, 255, 255, 0))
     img.putdata(newData)
     img.save(output_path, "PNG")
 
 folder = r"E:\zxz\project\shapes\a"
+output_folder = r"E:\zxz\project\shapes\c"
+
+
 for file in os.listdir(folder):
     if file.lower().endswith(".png"):
         input_path = os.path.join(folder, file)
-        output_path = os.path.join(r"E:\zxz\project\shapes\b",file)
-        remove_white_background(input_path, output_path)
+        output_path = os.path.join(output_folder, file)
+        keep_dark_lines_only(input_path, output_path, threshold=160)
